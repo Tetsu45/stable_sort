6ebca06aa8f52ba8994ee9a212be9c01562027b7
テイイ <148879729+Tetsu45@users.noreply.github.com>
2024-06-16 17:29:49 +0900

    good

--- stable.cpp
+++ stable.cpp
@@ -1,68 +1,60 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-int getSuitValue(char suit);
-void selection_sort(Card* cards,int* num_cards);
-bool compare_funtion(int* num1,int* num2);
-int* orderDet(int * arr);
 struct Card{
     char suit;
     int value;
 }
-bool read_input(){
-    int num_cards;
-    cin >> num_cards;
-    if (n< 1 || n > 36) return false;
-    Card *cards = new Card[num_cards];
-    for(int i = 0;i< num_cards; i++){
-        char input[3];//i used 
+int* read_input(int* num_cards){
+    Card *cards = new Card[*num_cards];
+    for(int i = 0;i< *num_cards; i++){
+        char input[3];
         cin.get(input,3);
         cin.get();//removes new line
         cards.suit = input[0];
         cards.value = input[1] - '0';//coversion of char into integer!
     } 
-    delete[] cards;
+    return cards;
 }
-void selection_sort(Card* cards,int* num_cards){
-    Card *selection_cards = new Card[*num_cards];
-    int *arr_index =  NULL;
-    int cnt = 0;
-    int cnt_capacity = 0;
-    for (int i = 0; i < *num_cards-1;i++){
+int* bubble_cards(Card *cards,int* num_cards){
+    Card* bubble_cards = new Card[*num_cards];
+    for (int ix = 0; ix < *num_cards; ix++) {
+    for (int jx = *num_cards - 1; jx >= ix + 1; jx--) {
+        if (arr[jx].value < arr[jx - 1].value) {
+            int temp = arr[jx - 1];
+            arr[jx - 1] = arr[jx];
+            arr[jx] = temp;
+        }
+    }
+}
+bubble_cards = cards;
+return bubble_cards
+}
+int* selection_cards(Card *cards,int* num_cards){
+    Card* selection_cards = new Card[*num_cards];
+    for(int i = 0;i < *num_cards ; i++){
         int min = i;
         for(int j = i+1;j<*num_cards;j++){
-            if (cards[j].value == cards[j-1].value){
-               
-            }
-            if (cards[j].value < cards[j-1].value){
+            if(cards[j].value < cards[j-1].value ){
                 min = j;
             }
-        }
-        if (min !=  i){
-            Card temp =  cards[min];
-            cards[min] = cards[i];
-            cards[i] = cards[min];
-            
+            if (min != i){
+                Card temp = cards[min];
+                cards[min] = cards[j-1];
+                cards[j-1] = temp;
+            }
         }
     }
-    for (int ix = 0; ix <*num_cards; ix++){
-        
-    }
-    delete[] arr_index;
-    delete[] selection_cards;
-}
-int getSuitValue(char suit){
-    switch(suit):
-    case 'S': return 0;
-    case 'H': return 1;
-    case 'C': return 2;
-    case 'D': return 3;
-    default : return -1;
-}
-bool compare_funtion(int* num1,int* num2){
-    if (num1 > num2) return true
-    else return false;
+    selection_cards = cards;
+    return  selection_cards;
 }
 int main(){
-    return -1;
+    int num_cards = 0
+    cin >> num_cards;
+    if (n< 1 || n > 36) return -1;
+    int* cards = read_input(&num_cards);
+    int* orginal = cards;
+    int* select_sorted =  selection_cards(cards,&num_cards);
+    int* bubble_sorted = bubble_cards(cards,&num_cards);
+    return 0;
 }

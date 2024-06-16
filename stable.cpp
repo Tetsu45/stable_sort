c03e411afc00984f3f8f1af93a2befdbb5232d67
テイイ <148879729+Tetsu45@users.noreply.github.com>
2024-06-16 09:44:02 +0000

    hopefull

--- stable.cpp
+++ stable.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstring>
-
 using namespace std;
 
 struct Card {
@@ -8,110 +7,112 @@ struct Card {
     int value;
 };
 
-Card* read_input(int& num_cards) {
-    cin >> num_cards;
-    if (num_cards < 1 || num_cards > 36) {
-        return nullptr;
-    }
-    Card* cards = new Card[num_cards];
-    for (int i = 0; i < num_cards; i++) {
+Card* read_input(int* num_cards) {
+    Card* cards = new Card[*num_cards];
+    for (int i = 0; i < *num_cards; i++) {
         char input[3];
         cin.get(input, 3);
-        cin.get(); // Remove the newline character
+        cin.get(); // removes new line
         cards[i].suit = input[0];
-        cards[i].value = input[1] - '0'; // Convert character to integer
+        cards[i].value = input[1] - '0'; // conversion of char into integer
     }
     return cards;
 }
 
-bool is_stable(Card* cards, Card* sorted_cards, int num_cards) {
+bool is_stable(Card* cards1, Card* cards2, int num_cards) {
+    int count[10] = {0};
+    for (int k = 0; k < num_cards; k++) count[cards1[k].value]++;
+    int index[10];
+    for (int h = 0; h < 10; h++) index[h] = count[h];
+    for (int z = 1; z < 10; z++) index[z] += index[z - 1];
+    Card* temp = new Card[num_cards];
+    for (int i = num_cards - 1; i >= 0; i--) {
+        temp[index[cards2[i].value] - 1] = cards2[i];
+        index[cards2[i].value]--;
+    }
+
     bool stable = true;
     for (int i = 0; i < num_cards - 1; i++) {
-        if (sorted_cards[i].value == sorted_cards[i + 1].value) {
-            if (cards[i].suit > cards[i + 1].suit) {
-                stable = false;
-                break;
-            }
+        if (temp[i].value == temp[i + 1].value && temp[i].suit < temp[i + 1].suit) {
+            stable = false;
+            break;
         }
     }
+    delete[] temp;
     return stable;
 }
 
-void bubble_sort(Card* cards, int num_cards) {
-    for (int i = 0; i < num_cards; i++) {
-        for (int j = num_cards - 1; j >= i + 1; j--) {
-            if (cards[j].value < cards[j - 1].value) {
-                Card temp = cards[j - 1];
-                cards[j - 1] = cards[j];
-                cards[j] = temp;
+Card* bubble_sort(Card* cards, int num_cards) {
+    Card* bubble_cards = new Card[num_cards];
+    memcpy(bubble_cards, cards, num_cards * sizeof(Card));
+    for (int ix = 0; ix < num_cards; ix++) {
+        for (int jx = num_cards - 1; jx > ix; jx--) {
+            if (bubble_cards[jx].value < bubble_cards[jx - 1].value) {
+                Card temp = bubble_cards[jx];
+                bubble_cards[jx] = bubble_cards[jx - 1];
+                bubble_cards[jx - 1] = temp;
             }
         }
     }
+    return bubble_cards;
 }
 
-void selection_sort(Card* cards, int num_cards) {
-    for (int i = 0; i < num_cards - 1; i++) {
-        int min = i;
+Card* selection_sort(Card* cards, int num_cards) {
+    Card* selection_cards = new Card[num_cards];
+    memcpy(selection_cards, cards, num_cards * sizeof(Card));
+    for (int i = 0; i < num_cards; i++) {
+        int min_index = i;
         for (int j = i + 1; j < num_cards; j++) {
-            if (cards[j].value < cards[min].value) {
-                min = j;
+            if (selection_cards[j].value < selection_cards[min_index].value) {
+                min_index = j;
             }
         }
-        if (min != i) {
-            Card temp = cards[min];
-            cards[min] = cards[i];
-            cards[i] = temp;
+        if (min_index != i) {
+            Card temp = selection_cards[i];
+            selection_cards[i] = selection_cards[min_index];
+            selection_cards[min_index] = temp;
         }
     }
+    return selection_cards;
 }
 
 int main() {
-    int num_cards;
-    Card* cards = read_input(num_cards);
-    if (cards == nullptr) {
-        cout << "Invalid input!" << endl;
-        return -1;
-    }
-
-    Card* bubble_sorted = new Card[num_cards];
-    Card* selection_sorted = new Card[num_cards];
-    memcpy(bubble_sorted, cards, num_cards * sizeof(Card));
-    memcpy(selection_sorted, cards, num_cards * sizeof(Card));
-
-    bubble_sort(bubble_sorted, num_cards);
-    selection_sort(selection_sorted, num_cards);
+    int num_cards = 0;
+    cin >> num_cards;
+    if (num_cards < 1 || num_cards > 36) return -1;
+    cin.ignore(); // to ignore the newline character after the number of cards input
+    Card* cards = read_input(&num_cards);
+    Card* bubble_sorted = bubble_sort(cards, num_cards);
+    Card* selection_sorted = selection_sort(cards, num_cards);
 
     for (int i = 0; i < num_cards; i++) {
         if (i == num_cards - 1) {
-            cout << bubble_sorted[i].suit << bubble_sorted[i].value << endl;
+            cout << bubble_sorted[i].suit << bubble_sorted[i].value << "\n";
         } else {
             cout << bubble_sorted[i].suit << bubble_sorted[i].value << " ";
         }
     }
-
     if (is_stable(cards, bubble_sorted, num_cards)) {
-        cout << "Stable" << endl;
+        cout << "Stable\n";
     } else {
-        cout << "Not stable" << endl;
+        cout << "Not stable\n";
     }
 
     for (int i = 0; i < num_cards; i++) {
         if (i == num_cards - 1) {
-            cout << selection_sorted[i].suit << selection_sorted[i].value << endl;
+            cout << selection_sorted[i].suit << selection_sorted[i].value << "\n";
         } else {
             cout << selection_sorted[i].suit << selection_sorted[i].value << " ";
         }
     }
-
     if (is_stable(cards, selection_sorted, num_cards)) {
-        cout << "Stable" << endl;
+        cout << "Stable\n";
     } else {
-        cout << "Not stable" << endl;
+        cout << "Not stable\n";
     }
 
     delete[] cards;
     delete[] bubble_sorted;
     delete[] selection_sorted;
-
     return 0;
 }
